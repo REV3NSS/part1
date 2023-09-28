f4eefc24223c4adfe6033c39bc2cbd99df537f14
Bruh <bruh@>
2023-09-28 12:36:09 +0200

    Update bruh

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,27 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { render } from 'react-dom'
 
+const part1 = 'Fundamentals of React'
+const exercises1 = 10
+const part2 = 'Using props to pass data'
+const exercises2 = 7
+const part3 = 'State of a component'
+const exercises3 = 14
+const course = 'Half Stack application development'
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-
   return (
     <div>
       <Header course={course} />
       <Content />
-      <Total />
+      
     </div>
   )
 }
 
-const Header = () => {
+const Header = ({course}) => {
   return (
     <h1>{course}</h1>
   )
@@ -30,33 +30,17 @@ const Header = () => {
 const Content = () => {
   return (
     <div>
-      <Part1 part1={part1} exercises1={exercises1} />
-      <Part2 part2={part2} exercises2={exercises2} />
-      <Part3 part3={part3} exercises3={exercises3} />
+      <Part part={part1} exercises={exercises1} />
+      <Part part={part2} exercises={exercises2} />
+      <Part part={part3} exercises={exercises3} />
     </div>
   )
 }
 
-const Part1 = () => {
+const Part = ({part, exercises}) => {
   return (
     <p>
-      {part1} {exercises1}
-    </p>
-  )
-}
-
-const Part2 = () => {
-  return (
-    <p>
-      {part2} {exercises2}
-    </p>
-  )
-}
-
-const Part3 = () => {
-  return (
-    <p>
-      {part2} {exercises2}
+      {part} {exercises}
     </p>
   )
 }
